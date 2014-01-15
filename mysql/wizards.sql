f03b42cb4c30504222843e0367cb12377d5c592c
nishacodes <nishabatra27@gmail.com>
2014-01-15 15:39:24 -0500

    answer sql questions

--- mysql/wizards.sql
+++ mysql/wizards.sql
@@ -50,10 +50,57 @@ ORDER BY id DESC limit 1;
 
 DROP TABLE wizards;
 
-
 -- Create two data models for wizards. The wizards are going to have a name, an age and two powers each. Let's pretend we're in a world where wizards have many powers, but powers belong to one wizard (two wizards cant have the same power) Each power is going to have a name and a damage amount. Write out the sql to create the two schemas and some sample records. What are the costs/benefits of the two approaches? If you only see one way of doing this that's ok. Please explain any potential problems with your approach. What questions can this schema answer easily? What questions can this schema not answer easily?
 
+DATA MODELS
+----------------
+wizards table:
+columns: 
+- name
+- age
+
+powers table:
+columns:
+- name
+- damage_amount
+- wizard_id
+
+wizard model:
+has_many :powers
+
+power model:
+belongs_to :wizard
+validates_uniqueness_of :wizard_name
+
+SQL COMMANDS
+----------------
+CREATE TABLE wizards(
+id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+name text,
+age integer);
+
+CREATE TABLE powers(
+id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+name text,
+damage_amount integer,
+wizard_id integer);
+
+INSERT INTO wizards (name, age)
+value ("nisha",28);
+
+INSERT INTO powers (name, damage_amount, wizard_id)
+value ("invisibility",500,1);
+
+SELECT *
+FROM wizards
+JOIN powers on powers.wizard_id = wizards.id;
+
 -- Retrieve all the wizards with powers that have the letter "l" in them.
+
+SELECT *
+FROM wizards
+JOIN powers on powers.wizard_id = wizards.id
+WHERE powers.name = "%l%";
 
 -- Retrieve all the wizards who have a combined damage of 10 or more.
 
@@ -69,6 +116,13 @@ DROP TABLE wizards;
 
 -- What's the difference between an outer and inner join?
 
+An inner join returns a table with only the rows that have a match in the join column. An outer join will return all the rows in both joined tables, even if some of them have no relation to the other table
+
 -- If I just write JOIN, is that an inner or outer join?
 
+inner join
+
 -- When would I want to use an outer join?
+
+When you want to see which rows don't have a relation to the other table and which do.
+
